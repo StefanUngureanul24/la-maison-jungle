28a9e7e46f8d6679a5d0fe8986c8a07fa01f363d
StefanUngureanul24 <unsugreanu_stefan_lucian@yahoo.com>
2022-03-20 17:15:41 +0100

    Creation of a button that hides or displays the shopping cart based on the local state's value

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+
 function Cart() {
+    /*
     const monsteraPrice = 8;
     const ivyPrice = 10;
     const fleursPrice = 15;
@@ -15,6 +18,30 @@ function Cart() {
     
             <p>Total : {monsteraPrice + ivyPrice + fleursPrice}€</p>
         </div>
+    )
+    */
+
+    const monsteraPrice = 8
+    const [cart, updateCart] = useState(0)
+    const [isOpen, setIsOpen] = useState(false)
+
+    return isOpen ? (
+        <div className='lmj-cart'>
+            <button onClick={() => setIsOpen(false)}>Close Shopping Cart</button>
+            <h2>Panier</h2>
+            <div>
+                Monstera: {monsteraPrice}€
+                <button onclick={() => updateCart(cart + 1)}>
+                    Add
+                </button>
+            </div>
+            <h3>Total : {monsteraPrice * cart}€</h3>
+            <button onClick={() => updateCart(0)}>
+                Empty shopping cart
+            </button>
+        </div>
+    ) : (
+        <button onClick={() => setIsOpen(true)}>Open Shopping Cart</button>
     )
 }
 
